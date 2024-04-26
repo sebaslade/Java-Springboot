5fa735003f503e08322054f4660adc296cfa1c5c
sebaslade <sebaslade29@gmail.com>
2024-04-26 18:37:15 -0500

    Agregando la funcionalidad Eliminar

--- controllers/UsuarioController.java
+++ controllers/UsuarioController.java
@@ -3,10 +3,7 @@ package com.cursojava.curso.controllers;
 import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +24,14 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "api/usuarios")
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
     public List<Usuario> getUsuarios(){
         return usuarioDao.getUsuarios();
+    }
+
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
+    public void registrarUsuarios(@RequestBody Usuario usuario){
+        usuarioDao.registrar(usuario);
     }
 
     @RequestMapping(value = "usuario1")
